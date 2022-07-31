7c8f95d69aa172ad793153c8b21b71dac4b93818
amanhgnis <amanhgnis.py@gmail.com>
2022-07-31 21:33:56 +0200

    Minor changes

--- src/algorithm_visualizer.py
+++ src/algorithm_visualizer.py
@@ -1,6 +1,5 @@
 import pygame
 from consts import *
-from algorithms import *
 import random
 
 class AlgorithmVisualizer:
@@ -89,6 +88,269 @@ class AlgorithmVisualizer:
         
         pygame.display.update()
         
+    def draw_menu(self):
+        x, y = 0, 0
+        width = self.screen.get_width()
+        height = self.screen.get_height()
+        menu_background = pygame.Rect(x, y, width, 3 * self.block_size)
+        pygame.draw.rect(self.screen, GREY, menu_background, 0)
+
+        reset_button = pygame.Rect(x + 1*self.block_size, y + 1*self.block_size, 2*self.block_size, self.block_size)
+        play_button = pygame.Rect(width//4 + 1*self.block_size, y + 1*self.block_size, 2*self.block_size, self.block_size)
+        selection_sort_button = pygame.Rect(width//2 + 1*self.block_size, y + 1*self.block_size, 5*self.block_size, self.block_size)
+        merge_sort_button = pygame.Rect(width//2  + 7*self.block_size, y + 1*self.block_size, 5*self.block_size, self.block_size)
+        quick_sort_button = pygame.Rect(width//2 + 13*self.block_size, y + 1*self.block_size, 5*self.block_size, self.block_size)
+
+        pygame.draw.rect(self.screen, BACKGROUD_COLOUR, reset_button, 1, border_radius=3)
+        pygame.draw.rect(self.screen, BACKGROUD_COLOUR, play_button, 1, border_radius=3)
+        pygame.draw.rect(self.screen, BACKGROUD_COLOUR, selection_sort_button, 1, border_radius=3)
+        pygame.draw.rect(self.screen, BACKGROUD_COLOUR, merge_sort_button, 1, border_radius=3)
+        pygame.draw.rect(self.screen, BACKGROUD_COLOUR, quick_sort_button, 1, border_radius=3)
+
+        text_rect = reset_button
+        text = self.font.render(f"RESET", True, BACKGROUD_COLOUR)
+        text_rect = text.get_rect()
+        text_rect.center = reset_button.center
+        self.screen.blit(text, text_rect)
+
+        text_rect = play_button
+        text = self.font.render(f"PLAY", True, BACKGROUD_COLOUR)
+        text_rect = text.get_rect()
+        text_rect.center = play_button.center
+        self.screen.blit(text, text_rect)
+
+        text_rect = selection_sort_button
+        text = self.font.render(f"SELECTION SORT", True, BACKGROUD_COLOUR)
+        text_rect = text.get_rect()
+        text_rect.center = selection_sort_button.center
+        self.screen.blit(text, text_rect)
+ 
+        text_rect = merge_sort_button
+        text = self.font.render(f"MERGE SORT", True, BACKGROUD_COLOUR)
+        text_rect = text.get_rect()
+        text_rect.center = merge_sort_button.center
+        self.screen.blit(text, text_rect)
+    
+        text_rect = reset_button
+        text = self.font.render(f"QUICK SORT", True, BACKGROUD_COLOUR)
+        text_rect = text.get_rect()
+        text_rect.center = quick_sort_button.center
+        self.screen.blit(text, text_rect)
+ 
+        pos = pygame.mouse.get_pos()
+        if reset_button.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0]:
+                self.reset_arr()
+        if play_button.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0]:
+                self.sort = True
+        if selection_sort_button.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0]:
+                self.algorithm = "selection sort"
+        if merge_sort_button.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0]:
+                self.algorithm = "merge sort"
+        if quick_sort_button.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0]:
+                self.algorithm = "quick sort"
+        
+        pygame.display.update()
+        
+    def draw_selection_sort(self, arr, order_array_len=0, i=-1, j=-1):
+        self.screen.fill(BACKGROUD_COLOUR)
+        index = 0
+        for x in range(self.block_size, self.block_size*(len(arr)+1), self.block_size):
+            y = self.screen.get_height() // 2 - 1*self.block_size
+            rect = pygame.Rect(x, y, self.block_size, self.block_size)
+            text_rect = rect
+            if index < order_array_len:
+                pygame.draw.rect(self.screen, GREEN, rect, 0)
+            if index == i:
+                pygame.draw.rect(self.screen, RED, rect, 0)
+            if index == j:
+                pygame.draw.rect(self.screen, BLUE, rect, 0)
+            pygame.draw.rect(self.screen, BLACK, rect, 1)
+            text = self.font.render(f"{arr[index]}", True, BLACK)
+            text_rect = text.get_rect()
+            text_rect.center = rect.center
+            self.screen.blit(text, text_rect)
+            index += 1
+        pygame.display.update()
+
+    def draw_merge_sort(self, arr, start=0, end=0, mid=-1, i=-1, j=-1, k=-1, arr_=[], left=[], right=[], all_green=False):
+        self.screen.fill(BACKGROUD_COLOUR)
+        index = 0
+        y = self.screen.get_height() // 2 - 1*self.block_size
+        for x in range(self.block_size, self.block_size*(len(arr)+1), self.block_size):
+            rect = pygame.Rect(x, y, self.block_size, self.block_size)
+            text_rect = rect
+            if not all_green:
+                if index > end:
+                    pygame.draw.rect(self.screen, GREY, rect, 0)
+                if index < start:
+                    pygame.draw.rect(self.screen, GREY, rect, 0)
+            else:
+                pygame.draw.rect(self.screen, GREEN, rect, 0)
+            pygame.draw.rect(self.screen, BLACK, rect, 1)
+
+            if (index < k  or index > end) or all_green:
+                pygame.draw.rect(self.screen, BLACK, rect, 1)
+                text = self.font.render(f"{arr[index]}", True, BLACK)
+                text_rect = text.get_rect()
+                text_rect.center = rect.center
+                self.screen.blit(text, text_rect)
+            index += 1
+
+
+        index = 0
+        for x in range(self.block_size, self.block_size*(len(arr_)+1), self.block_size):
+            rect_below = pygame.Rect(x, y + 2*self.block_size, self.block_size, self.block_size)
+            text_rect = rect_below 
+            pygame.draw.rect(self.screen, BLACK, rect_below, 1)
+            text = self.font.render(f"{arr_[index]}", True, BLACK)
+            text_rect = text.get_rect()
+            text_rect.center = rect_below.center
+            self.screen.blit(text, text_rect)
+            index += 1
+
+        index = 0
+        for x in range(self.block_size, self.block_size*(len(left)+1), self.block_size):
+            rect_below = pygame.Rect(x, y + 4*self.block_size, self.block_size, self.block_size)
+            text_rect = rect_below 
+            if index == i:
+                pygame.draw.rect(self.screen, RED, rect_below, 0)
+
+            pygame.draw.rect(self.screen, BLACK, rect_below, 1)
+            text = self.font.render(f"{left[index]}", True, BLACK)
+            text_rect = text.get_rect()
+            text_rect.center = rect_below.center
+            self.screen.blit(text, text_rect)
+            index += 1
+
+        index = 0
+        for x in range(self.block_size, self.block_size*(len(right)+1), self.block_size):
+            rect_below = pygame.Rect(x, y + 6*self.block_size, self.block_size, self.block_size)
+            text_rect = rect_below 
+            if index == j:
+                pygame.draw.rect(self.screen, BLUE, rect_below, 0)
+            pygame.draw.rect(self.screen, BLACK, rect_below, 1)
+            text = self.font.render(f"{right[index]}", True, BLACK)
+            text_rect = text.get_rect()
+            text_rect.center = rect_below.center
+            self.screen.blit(text, text_rect)
+            index += 1
+
+        pygame.display.update()
+
+    def draw_quick_sort(self, arr, start=-1, end=-1, pi=-1, all_green=False):
+        self.screen.fill(BACKGROUD_COLOUR)
+        index = 0
+        for x in range(self.block_size, self.block_size*(len(arr)+1), self.block_size):
+            y = self.screen.get_height() // 2 - 1*self.block_size
+            rect = pygame.Rect(x, y, self.block_size, self.block_size)
+            text_rect = rect
+            if not all_green:
+                if index == start:
+                    pygame.draw.rect(self.screen, RED, rect, 0)
+                if index == end:
+                    pygame.draw.rect(self.screen, BLUE, rect, 0)
+                if index == pi:
+                    pygame.draw.rect(self.screen, GREEN, rect, 0)
+            else:
+                pygame.draw.rect(self.screen, GREEN, rect, 0)
+            pygame.draw.rect(self.screen, BLACK, rect, 1)
+            text = self.font.render(f"{arr[index]}", True, BLACK)
+            text_rect = text.get_rect()
+            text_rect.center = rect.center
+            self.screen.blit(text, text_rect)
+            index += 1
+        pygame.display.update()
+
+    def selection_sort(self, arr):
+        ordered_array_len = 0
+        for i in range(len(arr)):
+            min_index = i
+            for j in range(i+1, len(arr)):
+                if arr[j] <= arr[min_index]:
+                    min_index = j
+                self.draw_selection_sort(arr, ordered_array_len,i, j)
+                pygame.time.wait(self.delay)
+            if min_index != i:
+                arr[i], arr[min_index] = arr[min_index], arr[i]
+            ordered_array_len += 1
+            pygame.time.wait(self.delay)
+
+    def merge(self, arr, start, end, mid):
+        left = arr[start:mid+1]
+        right = arr[mid+1:end+1]
+        arr_ = arr[start:end+1]
+        i, j, k = 0, 0, start
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                self.draw_merge_sort(arr, start, end,mid, i, j, k, arr_, left, right)
+                pygame.time.wait(self.delay)
+                i += 1
+                k += 1
+            else:
+                arr[k] = right[j]
+                self.draw_merge_sort(arr, start, end,mid, i, j, k, arr_, left, right)
+                pygame.time.wait(self.delay)
+                j += 1
+                k += 1
+        while i < len(left):
+            arr[k] = left[i]
+            self.draw_merge_sort(arr, start, end,mid, i, j, k, arr_, left, right)
+            pygame.time.wait(self.delay)
+            i +=1
+            k += 1
+        while j < len(right):
+            arr[k] = right[j]
+            self.draw_merge_sort(arr, start, end,mid, i, j, k, arr_, left, right)
+            pygame.time.wait(self.delay)
+            j +=1
+            k += 1
+        self.draw_merge_sort(arr, start, end,mid, i, j, k, arr_, left, right)
+        pygame.time.wait(self.delay)
+        return arr
+            
+    def merge_sort(self, arr, start, end):
+        if end <= start:
+            return arr
+        mid  = start + (end-start)// 2
+        self.merge_sort(arr, start, mid)
+        self.merge_sort(arr, mid+1, end)
+        self.merge(arr, start, end, mid)
+        return arr
+
+    def partition(self, arr, start, end):
+        pivot_index = start
+        pivot = arr[start]
+        while start < end:
+            while start < len(arr) and arr[start] <= pivot:
+                self.draw_quick_sort(arr, start, end, pi=pivot_index)
+                pygame.time.wait(self.delay)
+                start += 1
+            while arr[end] > pivot:
+                self.draw_quick_sort(arr, start, end, pi=pivot_index)
+                pygame.time.wait(self.delay)
+                end -= 1
+            if start < end:
+                self.draw_quick_sort(arr, start, end, pi=pivot_index)
+                pygame.time.wait(self.delay)
+                arr[start], arr[end] = arr[end], arr[start]
+        arr[pivot_index], arr[end] = arr[end], arr[pivot_index]
+        self.draw_quick_sort(arr, start, end, pi=pivot_index)
+        pygame.time.wait(self.delay)
+        return end
+
+    def quick_sort(self, arr, start, end):
+        if start < end:
+            pi = self.partition(arr, start, end)
+            self.quick_sort(arr, start, pi-1)
+            self.quick_sort(arr, pi + 1, end)
+        return arr
+
     def run(self):
         while self.running:
             self.clock.tick(self.frame_rate)
@@ -109,20 +371,17 @@ class AlgorithmVisualizer:
             if self.sort:
                 if sorted(self.arr) != self.arr:
                     if self.algorithm == "selection sort":
-                        selection_sort(self.arr, delay=self.delay)
+                        self.selection_sort(self.arr)
                     elif self.algorithm == "merge sort":
-                        merge_sort(self.arr, 0, len(self.arr)-1, delay=self.delay)
+                        self.merge_sort(self.arr, 0, len(self.arr)-1)
                     else:
-                        quick_sort(self.arr, 0, len(self.arr)-1, delay=self.delay)
+                        self.quick_sort(self.arr, 0, len(self.arr)-1)
                 else:
-                    draw_quick_sort(self.arr, all_green=True, delay=self.delay)
+                    self.draw_quick_sort(self.arr, all_green=True)
                     self.draw_menu()
             else:
-                draw_quick_sort(self.arr, all_green=False, delay=self.delay)
+                self.draw_quick_sort(self.arr, all_green=False)
                 self.draw_menu()
-            
-
-
 
 
 def main():
